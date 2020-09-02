e7a410ef34e356f9e1245e18016fd75d414c1c54
김성중 <s3c2@naver.com>
2020-09-02 23:48:11 +0900

    200902

---
+++ 0902/partsum.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('test.txt', 'r')
+T = int(input())
+for tc in range(T):
+    N, M = map(int,input().split())
+    data = list(map(int,input().split()))
+    line = []
+    for i in range(len(data)-M+1):
+        A = 0
+        for j in range(M):
+            A = A + data[i+j]
+        line.append(A)
+    line.sort()
+    print("#{} {}".format(tc+1, line[-1]-line[0]))  
